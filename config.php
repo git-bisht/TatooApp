5c7ec20ca8d90e29c49ba6c6adb16663f16837f1
git-bisht <mail.bisht1995@gmail.com>
2023-02-26 19:20:46 +0530

    first commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$localhost = "localhost";
+$username = "black-opal-ink-tatoo";
+$password = "Ij#Z&{GkV[.R";
+$db_name = "black-opal-ink-tatoo";
+
+
+
+$conn = mysqli_connect($localhost,$username,$password,$db_name) or die("Query failed".mysqli_error());
+
+?>
